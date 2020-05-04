187f61bce4481867ca1bb5bb647f3c8e523d4aae
aerth <aerth@riseup.net>
2020-05-04 00:42:46 +0000

    truncate average

--- system/status.go
+++ system/status.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"encoding/json"
+	"math"
 	"net/http"
 	"time"
 )
@@ -12,7 +13,7 @@ func (s *System) StatusHandler(w http.ResponseWriter, r *http.Request) {
 	if !stats.t1.IsZero() {
 		d := time.Since(s.Stats.t1)
 		stats.Uptime = d.Truncate(time.Second).Seconds()
-		stats.Average = float64(stats.Hits) / d.Seconds()
+		stats.Average = math.Round(float64(stats.Hits)/stats.Uptime*100) / 100
 	}
 	json.NewEncoder(w).Encode(stats)
 }
